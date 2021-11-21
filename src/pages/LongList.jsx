01d0afecfe3ee5e571bfef818782c7fbb7274438
holynova <holy_nova@126.com>
2021-11-21 17:56:04 +0800

    完成列表初版

--- src/pages/LongList.jsx
+++ src/pages/LongList.jsx
@@ -1,16 +1,68 @@
-import React, { useState, useEffect } from "react";
-import "./LongList.css";
+import React, { useState, useEffect, useRef } from "react";
+import "./LongList.scss";
 import { Button } from "antd";
 import { log } from "../common/debug";
 
+const total = 2e2;
+const itemHeight = 40;
+const wrapperHeight = 600;
+const pageSize = Math.ceil(wrapperHeight / itemHeight);
+
 const getList = (offset = 0, limit = 10) => {
+  let maxStartIndex = total - limit;
+  if (offset > maxStartIndex) {
+    offset = maxStartIndex;
+  }
   return new Array(limit).fill(0).map((_, index) => {
     return { id: index + offset, value: Math.random() };
   });
 };
 
 function LongList() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(
+    getList(0, Math.ceil(wrapperHeight / itemHeight))
+  );
+  const [offset, setOffset] = useState(0);
+  const blankRef = useRef(null);
+  const wrapperRef = useRef(null);
+  const viewRef = useRef(null);
+  useEffect(() => {
+    function initDOM() {
+      console.log("blank", blankRef.current);
+      if (blankRef.current) {
+        blankRef.current.style.height = total * itemHeight + "px";
+      }
+      if (viewRef.current) {
+        viewRef.current.style.marginTop = `${-total * itemHeight + 0}px`;
+      }
+    }
+
+    function onScroll() {
+      let scrollTop = wrapperRef.current.scrollTop;
+      let newOffset = Math.floor(scrollTop / itemHeight);
+      setOffset(newOffset);
+      let noMore = newOffset > total - pageSize;
+      if (!noMore) {
+        viewRef.current.style.marginTop = `${
+          -total * itemHeight + scrollTop
+        }px`;
+      }
+      console.log("scroll", `top=${scrollTop},offset=${newOffset}`);
+    }
+    function initEvent() {
+      if (wrapperRef.current) {
+        wrapperRef.current.addEventListener("scroll", onScroll);
+      }
+    }
+
+    initDOM();
+    initEvent();
+    return () => {
+      if (wrapperRef.current) {
+        wrapperRef.current.removeEventListener("scroll", onScroll);
+      }
+    };
+  }, []);
 
   // useEffect(()=>{
   //   const initWorker = () =>{
@@ -27,22 +79,37 @@ function LongList() {
 
   //   initWorker()
   // },[])
+  // useEffect(() => {
+  //   setList(getList(0, 100));
+  // }, []);
+
+  const viewListPart = list.map((x) => {
+    return (
+      <div
+        className="list-item"
+        key={x.id}
+        style={{ backgroundColor: x.id % 2 === 0 ? "lightgreen" : "auto" }}
+      >
+        <span>id = {x.id}</span>
+        <span>value = {x.value}</span>
+      </div>
+    );
+  });
+
   useEffect(() => {
-    setList(getList(0, 100));
-  }, []);
+    const newList = getList(offset, Math.ceil(wrapperHeight / itemHeight));
+    setList(newList);
+  }, [offset]);
 
   return (
     <div className="LongList">
       <h1>LongList</h1>
-      <div className="list-wrapper">
-        {list.map((x) => {
-          return (
-            <div className="list-item" key={x.id}>
-              <span>id = {x.id}</span>
-              <span>value = {x.value}</span>
-            </div>
-          );
-        })}
+      <h3>offset={offset}</h3>
+      <div className="list-wrapper" ref={wrapperRef}>
+        <div className="blank-part" ref={blankRef}></div>
+        <div className="view-part" ref={viewRef}>
+          {viewListPart}
+        </div>
       </div>
       {/* <Button>click</Button> */}
     </div>
